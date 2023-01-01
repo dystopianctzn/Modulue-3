eaf495ad15a4b6ae7c402b0af8e07df32e4a4d3e
dystopianctzn <kinda.toxic11@gmail.com>
2023-01-01 22:49:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Jane";
+
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
